8688b9ee4fea30731790c0a28376f15801fa6aef
JaredMel <jareddmelendez1764@gmail.com>
2024-11-23 13:08:39 -0800

    WIP

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 const int SIZE = 7;
+
+void DFSRec(vector<vector<Pair>>&, vector<bool>&, int);
+void DFS(vector<vector<Pair>>&, int);
+void bfs(vector<vector<Pair>>&, int);
 
 struct Edge {
     int src, dest, weight;
@@ -57,6 +62,66 @@ int main() {
 
     // Prints adjacency list representation of graph
     graph.printGraph();
-    
+
+    cout << "DFS starting from vertex 0:" << endl;
+    DFS(graph.adjList, 0);
+
     return 0;
+}
+
+// Recursive function for DFS traversal
+void DFSRec(vector<vector<Pair>> &adj, vector<bool> &visited, int s){
+  
+    visited[s] = true;
+
+    // Print the current vertex
+    cout << s << " ";
+
+    // Recursively visit all adjacent vertices
+    // that are not visited yet
+    for (Pair i : adj[s])
+        if (visited[i] == false)
+            DFSRec(adj, visited, i);
+}
+
+// Main DFS function that initializes the visited array
+// and call DFSRec
+void DFS(vector<vector<Pair>> &adj, int s){
+    vector<bool> visited(adj.size(), false);
+    DFSRec(adj, visited, s);
+}
+
+void bfs(vector<vector<Pair>>& adj, int s) 
+{
+    // Create a queue for BFS
+    queue<int> q;  
+    
+    // Initially mark all the vertices as not visited
+    // When we push a vertex into the q, we mark it as 
+    // visited
+    vector<bool> visited(adj.size(), false);
+
+    // Mark the source node as visited and 
+    // enqueue it
+    visited[s] = true;
+    q.push(s);
+
+    // Iterate over the queue
+    while (!q.empty()) {
+      
+        // Dequeue a vertex from queue and print it
+        int curr = q.front();
+        q.pop();
+        cout << curr << " ";
+
+        // Get all adjacent vertices of the dequeued 
+        // vertex curr If an adjacent has not been 
+        // visited, mark it visited and enqueue it
+        for (int x : adj[curr]) {
+            if (!visited[x]) {
+                visited[x] = true;
+                q.push(x);
+            }
+        }
+    }
 }
