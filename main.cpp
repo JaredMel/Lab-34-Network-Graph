2a977037a1338a839972bdc829c4b675d75a4a58
JaredMel <jareddmelendez1764@gmail.com>
2024-11-24 09:06:57 -0800

    WIP

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ void DFSRec(vector<vector<Pair>>&, vector<bool>&, int);
 void DFS(vector<vector<Pair>>&, int);
 void bfs(vector<vector<Pair>>&, int);
 void shortestPath(vector<vector<Pair>>&, int);
+void dijkstra(vector<vector<Pair>>&, int);
+int minDistance(int[], bool[]);
 
 class Graph {
 public:
@@ -156,4 +158,64 @@ void shortestPath(vector<vector<Pair>>& adj, int s)
             for (Pair v : adj[i])
                 cout << " -> Point " << v.first << " (Pacing: " << v.second << " km)" << endl;
         }
+}
+
+void dijkstra(vector<vector<Pair>>& adj, int src)
+{
+    int dist[SIZE]; // The output array.  dist[i] will hold the
+                 // shortest
+    // distance from src to i
+
+    bool sptSet[SIZE]; // sptSet[i] will be true if vertex i is
+                    // included in shortest
+    // path tree or shortest distance from src to i is
+    // finalized
+
+    // Initialize all distances as INFINITE and stpSet[] as
+    // false
+    for (int i = 0; i < SIZE; i++)
+        dist[i] = INT_MAX, sptSet[i] = false;
+
+    // Distance of source vertex from itself is always 0
+    dist[src] = 0;
+
+    // Find shortest path for all vertices
+    for (int count = 0; count < SIZE - 1; count++) {
+        // Pick the minimum distance vertex from the set of
+        // vertices not yet processed. u is always equal to
+        // src in the first iteration.
+        int u = minDistance(dist, sptSet);
+
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
+
+        // Update dist value of the adjacent vertices of the
+        // picked vertex.
+        for (int v = 0; v < SIZE; v++)
+
+            // Update dist[v] only if is not in sptSet,
+            // there is an edge from u to v, and total
+            // weight of path from src to  v through u is
+            // smaller than current value of dist[v]
+            if (!sptSet[v] && adj[u][v].second
+                && dist[u] != INT_MAX
+                && dist[u] + adj[u][v].second < dist[v])
+                dist[v] = dist[u] + adj[u][v].second;
+    }
+
+    // print the constructed distance array
+    
+}
+
+int minDistance(int dist[], bool sptSet[])
+{
+
+    // Initialize min value
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < SIZE; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+
+    return min_index;
 }
