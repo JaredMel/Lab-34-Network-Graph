0d11bfe6c684fc200b53bae5373f989e83948ed8
JaredMel <jareddmelendez1764@gmail.com>
2024-11-24 09:24:03 -0800

    WIP

--- main.cpp
+++ main.cpp
@@ -18,6 +18,52 @@ void bfs(vector<vector<Pair>>&, int);
 void shortestPath(vector<vector<Pair>>&, int);
 int minDistance(int[], bool[]);
 
+class DSU { 
+    int* parent; 
+    int* rank; 
+  
+public: 
+    DSU(int n) 
+    { 
+        parent = new int[n]; 
+        rank = new int[n]; 
+  
+        for (int i = 0; i < n; i++) { 
+            parent[i] = -1; 
+            rank[i] = 1; 
+        } 
+    } 
+  
+    // Find function 
+    int find(int i) 
+    { 
+        if (parent[i] == -1) 
+            return i; 
+  
+        return parent[i] = find(parent[i]); 
+    } 
+  
+    // Union function 
+    void unite(int x, int y) 
+    { 
+        int s1 = find(x); 
+        int s2 = find(y); 
+  
+        if (s1 != s2) { 
+            if (rank[s1] < rank[s2]) { 
+                parent[s1] = s2; 
+            } 
+            else if (rank[s1] > rank[s2]) { 
+                parent[s2] = s1; 
+            } 
+            else { 
+                parent[s2] = s1; 
+                rank[s1] += 1; 
+            } 
+        } 
+    } 
+};
+
 class Graph {
 public:
     // a vector of vectors of Pairs to represent an adjacency list
@@ -51,6 +97,34 @@ public:
         }
         cout << endl;
     }
+
+    void kruskals_mst() 
+    { 
+        // Sort all edges 
+        sort(adjList.begin(), adjList.end()); 
+  
+        // Initialize the DSU 
+        DSU s(V); 
+        int ans = 0; 
+        cout << "Following are the edges in the "
+                "constructed MST"
+             << endl; 
+        for (auto edge : adjList) { 
+            int w = edge[0]; 
+            int x = edge[1]; 
+            int y = edge[2]; 
+  
+            // Take this edge in MST if it does 
+            // not forms a cycle 
+            if (s.find(x) != s.find(y)) { 
+                s.unite(x, y); 
+                ans += w; 
+                cout << x << " -- " << y << " == " << w 
+                     << endl; 
+            } 
+        } 
+        cout << "Minimum Cost Spanning Tree: " << ans; 
+    } 
 };
 
 int main() {
